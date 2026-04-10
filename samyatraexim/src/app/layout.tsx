c2526173c4c11ccf8e6512ab4b256941b102766f
siddh7 <smp18773@gmail.com>
2026-04-10 18:43:35 +1000

    Latest commit

--- samyatraexim/src/app/layout.tsx
+++ samyatraexim/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
@@ -31,9 +31,8 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <SpeedInsights />
-        <Header />
         {children}
-        </body>
+      </body>
     </html>
   );
 }
--- samyatraexim/src/app/page.tsx
+++ samyatraexim/src/app/page.tsx
@@ -1,10 +1,23 @@
+import Link from "next/link";
 import pageConfig from "./pageConfig.json";
-import componentRegistry from "./componentRegistry";
- 
+// import componentRegistry from "./componentRegistry";
+import Logo from "../../public/samyatraexim_logo.png";
+
+export const metadata = {
+  title: 'Samyatra Exim - Global Trade Solutions',
+  description: 'Samyatra Exim is a leading export-import company with a strong presence in the global market.',
+  openGraph: {
+    title: 'Samyatra Exim - Global Trade Solutions',
+    description: 'Samyatra Exim is a leading export-import company with a strong presence in the global market.',
+    url: 'https://samyatraexim.com/',
+    images: [{ url: Logo }],
+  },
+};
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
-      {pageConfig.map((section) => {
+      {/* {pageConfig.map((section) => {
         const Component = componentRegistry[section.component];
  
         if (!Component) {
@@ -13,7 +26,16 @@ export default function Home() {
         }
  
         return <Component key={section.id} {...section.props} />;
-      })}
+      })} */}
+      <div className={`flex flex-col justify-center items-center h-screen`}>
+        <h1 className={`uppercase text-4xl font-bold text-center my-8 text-black w-full`}>Samyatra Exim</h1>
+        <div className={`flex flex-col items-center w-full`}>
+          <h3 className={`text-black font-semibold`}>Contact Us:</h3>
+          <Link className={`text-black font-semibold hover:underline`} href="mailto:contact@samyatraexim.com">
+            contact@samyatraexim.com
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
